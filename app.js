38087a0deb4cf612bfeb7a905c05d16cb5258a4d
Roger B <myvarequals@gmail.com>
2018-11-25 02:36:12 -0500

    starting notes app

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 console.log('Running...');
 
 const fs = require('fs'); // fetch file system module
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes');
 
-let user = os.userInfo();
-console.log(user);
-// append text to file
-fs.appendFile('greetings.txt', `Hello ${user.username}`, err => {
-    if (err) { // catch error
-        console.log('Unable to write to file');
-    }
-});
+console.log(process.argv); // arguments vector
+const command = process.argv[2];
+console.log('Command:', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if(command === 'list') {
+    console.log('List all notes');
+} else if(command === 'read') {
+    console.log('Read note');
+} else if(command === 'remove') {
+    console.log('Remove note')
+} else {
+    console.log('Command not recognized!');
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,7 @@
 console.log('Running notes.js');
+// adding to export object to be accessed by other files
+
+module.exports.addNote = () => {
+    console.log('addNote called');
+    return 'New Note added';
+};
